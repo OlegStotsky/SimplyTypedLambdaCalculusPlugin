789cb6f79a65530d331399cc0dfeabd697f2b6e4
Oleg Stotsky <ostotsky@gmail.com>
2019-11-08 21:53:36 +0300

    add correct multiline error highlight displayment

--- src/main/java/com/olegstotsky/simplytypedlambdacalculus/TypingErrorHighlighterExternal.java
+++ src/main/java/com/olegstotsky/simplytypedlambdacalculus/TypingErrorHighlighterExternal.java
@@ -2,28 +2,26 @@ package com.olegstotsky.simplytypedlambdacalculus;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.process.*;
+import com.intellij.execution.process.CapturingProcessHandler;
+import com.intellij.execution.process.ProcessOutput;
+import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.ExternalAnnotator;
+import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.util.ProgressIndicatorBase;
-import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.source.tree.RecursiveTreeElementWalkingVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.olegstotsky.simplytypedlambdacalculus.psi.SimplyTypedLambdaCalculusStatement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.PrintWriter;
-import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class TypingErrorHighlighterExternal extends ExternalAnnotator<List<Pair<String, Integer>>, List<Pair<TextRange, String>>> {
@@ -77,28 +75,22 @@ public class TypingErrorHighlighterExternal extends ExternalAnnotator<List<Pair<
             return Optional.of(lines.get(2).substring(2));
         }
         if (lines.get(2).contains("Bad function application")) {
-            return Optional.of(parseBadFunctionApplication(lines));
+            return Optional.of(parseErrors(lines, 3));
         }
         if (lines.get(2).contains("Bad arith operand")) {
-            return Optional.of(parseBadArithOperands(lines));
+            return Optional.of(parseErrors(lines, 3));
         }
         if (lines.get(2).contains("Bad conditional")) {
-            return Optional.of(parseBadConditional(lines));
+            return Optional.of(parseErrors(lines, 4));
         }
 
         return Optional.empty();
     }
 
-    private String parseBadConditional(List<String> lines) {
-        return String.join("\n", lines.subList(2, 7));
-    }
-
-    private String parseBadArithOperands(List<String> lines) {
-        return String.join("\n", lines.subList(2, 6));
-    }
-
-    private String parseBadFunctionApplication(List<String> lines) {
-        return String.join("\n", lines.subList(2, 6));
+    private String parseErrors(List<String> lines, int numElements) {
+        List<String> errors = lines.subList(2, 2 + numElements);
+        errors.set(0, errors.get(0).substring(2));
+        return "<html>" + String.join("<br>", errors) + "</html>";
     }
 
     @Override
@@ -106,7 +98,7 @@ public class TypingErrorHighlighterExternal extends ExternalAnnotator<List<Pair<
         annotationResult.forEach(result -> {
             TextRange range = result.getFirst();
             String errorMessage = result.getSecond();
-            holder.createErrorAnnotation(range, errorMessage);
+            Annotation annotation = holder.createAnnotation(HighlightSeverity.ERROR, range, "", errorMessage);
         });
     }
 }
