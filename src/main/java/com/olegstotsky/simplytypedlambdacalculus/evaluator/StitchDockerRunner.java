7afda2ea69e2c88d69ce0250763e59f6e2eedd29
Oleg Stotsky <ostotsky@gmail.com>
2019-12-20 01:03:34 +0300

    add StitchDockerRunner

---
+++ src/main/java/com/olegstotsky/simplytypedlambdacalculus/evaluator/StitchDockerRunner.java
@@ -0,0 +1,39 @@
+package com.olegstotsky.simplytypedlambdacalculus.evaluator;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.CapturingProcessHandler;
+import com.intellij.execution.process.ProcessOutput;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
+
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Optional;
+
+public class StitchDockerRunner implements StitchRunner {
+    private static Logger LOG = Logger.getInstance(StitchDockerRunner.class);
+
+    @Override
+    public Optional<List<String>> evaluate(String text) {
+        GeneralCommandLine cmd = new GeneralCommandLine();
+        cmd.setExePath("docker");
+        cmd.addParameters("container", "exec", "-i", "d0", "/scripts/my_stitch_bin");
+        try {
+            Application app = ApplicationManager.getApplication();
+            CapturingProcessHandler processHandler = new CapturingProcessHandler(cmd);
+            PrintWriter stitchStdIn = new PrintWriter(processHandler.getProcessInput());
+            stitchStdIn.println(text.substring(0, text.length() - 1));
+            stitchStdIn.flush();
+            stitchStdIn.println(":quit");
+            stitchStdIn.flush();
+            ProcessOutput out = processHandler.runProcess();
+            List<String> lines = out.getStdoutLines();
+            return Optional.of(lines);
+        } catch (ExecutionException e) {
+            LOG.warn(e);
+            return Optional.empty();
+        }
+    }
+}
